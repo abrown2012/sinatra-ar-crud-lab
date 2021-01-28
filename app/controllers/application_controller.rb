bf63a28e3302a02a05ff7de5fb632787b2e5119e
Agnieszka Brown <abrown2012@icloud.com>
2021-01-27 20:39:24 -0500

    commit tree 7595bb9797a85c9e39415ff97794824f60a35971

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end 
+
+  get '/articles/:id/edit' do 
+    find_article
+    erb :edit 
+  end 
+
+  post '/articles' do 
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{Article.last.id}"
+  end 
+
+  patch '/articles/:id' do
+    find_article
+    @article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"   
+  end 
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show 
+  end 
+  
+  delete '/articles/:id' do
+    find_article
+    @article.destroy
+    redirect "/articles"
+  end
+
+  def find_article
+    @article = Article.find(params["id"])
   end
 end
